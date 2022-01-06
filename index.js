42ba241dd485c345c4f1ae3580b0b9aefba75ebb
= <maria.louisa.castro@gmail.com>
2022-01-06 17:52:49 -0600

    Added JS to inject colour code to HTML using .innerHTML

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ function generatecode() {
     code[i] = characters[Math.floor(Math.random() * 16)];
     i = i + 1;
   } while (i < 6);
-  return code.join("");
+  return "#" + code.join("");
 }
 
 let colourOne = generatecode();
 let colourTwo = generatecode();
 let colourThree = generatecode();
 let colourFour = generatecode();
-let colourOnFive = generatecode();
+let colourFive = generatecode();
 
-console.log(colourOne);
+//document.getElementById("color-1").style.backgroundColor = "lightblue";
+
+document.getElementById("text-1").innerHTML = colourOne;
+document.getElementById("text-2").innerHTML = colourTwo;
+document.getElementById("text-3").innerHTML = colourThree;
+document.getElementById("text-4").innerHTML = colourFour;
+document.getElementById("text-5").innerHTML = colourFive;
